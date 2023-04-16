1383c0a27e4f41ea30698ec256ed7dc6f1288c91
b-badr001 <128297042+b-badr001@users.noreply.github.com>
2023-04-16 10:24:16 +0000

    Add files via upload

---
+++ Hospital/src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -0,0 +1,71 @@
+package ma.enset.hospital;
+
+import ma.enset.hospital.entities.*;
+import ma.enset.hospital.repository.ConsultationRepository;
+import ma.enset.hospital.repository.MedecinRepository;
+import ma.enset.hospital.repository.PatientRepository;
+import ma.enset.hospital.repository.RendezVousRepository;
+import ma.enset.hospital.service.IHospitalService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(IHospitalService hospitalService){
+        return args -> {
+
+            Stream.of("Aymane","Ayoub","Hamza")
+                    .forEach(name->{
+                        Medecin medecin = new Medecin();
+                        medecin.setNom(name);
+                        medecin.setEmail(name+"@gmail.com");
+                        medecin.setSpecialite(Math.random()>0.5?"Cardio":"Dentiste");
+                        hospitalService.saveMedecin(medecin);
+                    });
+            Stream.of("Mohamed","Hassan","Najat")
+                    .forEach(name->{
+                        Patient patient = new Patient();
+                        patient.setNom(name);
+                        patient.setDatenaissance(new Date());
+                        patient.setMalade(false);
+                        hospitalService.savePatient(patient);
+                    });
+
+            Patient patient = patientRepository.findById(1L).orElse(null);
+            Patient patient1 = patientRepository.findByNom("Mohamed");
+
+            Medecin medecin = medecinRepository.findByNom("Ayoub");
+
+            RendezVous rendezVous = new RendezVous();
+            rendezVous.setDate(new Date());
+            rendezVous.setStatus(StatusRDV.PENDING);
+            rendezVous.setMedecin(medecin);
+            rendezVous.setPatient(patient);
+            hospitalService.saveRendezVous(rendezVous);
+
+            RendezVous rendezVous1=rendezVousRepository.findById(1L).orElse(null);
+            Consultation consultation = new Consultation();
+            consultation.setDateConsultation(new Date());
+            consultation.setRendezVous(rendezVous1);
+            consultation.setRapport("Rapport de la consulation .....");
+            hospitalService.saveConsultation(consultation);
+
+
+
+        };
+    }
+
+}
